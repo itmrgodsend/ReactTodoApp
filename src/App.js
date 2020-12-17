5daf79cf10b08fe9defd404f9eb17ff15065887e
itmrgodsend <itmrgodsend@gmail.com>
2020-12-17 22:07:38 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import TodoContainer from "./Components/TodoContainer";
+import {Provider} from "react-redux";
 
-const App = () => {
+
+
+const App = (props) => {
 
     return (
         <div className='wrapper'>
+
+
+                {console.log(store)}
+
+
             <TodoContainer/>
         </div>
     )
 }
+
+
 
 export default App;
