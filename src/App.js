83463985ac9f770cb7d29dbf9da98eb5e7561ff4
itmrgodsend <itmrgodsend@gmail.com>
2020-12-18 15:18:59 +0300

    fix connect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import TodoContainer from "./Components/TodoContainer";
-import {Provider} from "react-redux";
-
+import {connect} from "react-redux";
 
 
 const App = (props) => {
@@ -9,15 +8,19 @@ const App = (props) => {
     return (
         <div className='wrapper'>
 
+            {props.state.todos.length ? <TodoContainer/> : <p>No todos</p>}
 
-                {console.log(store)}
-
-
-            <TodoContainer/>
         </div>
     )
 }
 
+function mapStateToProps(state) {
+
+    return {state}
+
+}
+
+export default connect(mapStateToProps)(App);
 
 
-export default App;
+
